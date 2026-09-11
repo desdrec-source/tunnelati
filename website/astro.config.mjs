dda34ad4cb28a0c1e95aaa07fad37e5b1d3a130d
desdrec-source <desdrec@googlemail.com>
2026-09-11 08:54:00 +0100

    SEO: www canonical, homepage snippet, 301 duplicate Prufrock slug

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,6 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://tunnelati.com",
+  site: "https://www.tunnelati.com",
+  trailingSlash: "never",
   integrations: [sitemap()],
+  redirects: {
+    "/articles/prufrock-autonomous-rings-2026-08-08":
+      "/articles/2026-08-08-boring-company-details-prufrock-autonomous-ring-placement",
+    "/articles/prufrock-autonomous-rings-2026-08-08/":
+      "/articles/2026-08-08-boring-company-details-prufrock-autonomous-ring-placement",
+  },
 });
